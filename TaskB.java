9f47133758ee9b4282b8e055bdb997b4754c8269
JasonH17701 <jasonhuynh@cpp.edu>
2025-01-25 10:30:28 -0800

    Initial Commit

---
+++ TaskB.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class TaskB {    
+    public static void main(String[] args) throws IOException
+    {
+    
+        for(int i = 5; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                System.out.print ("\\o/");
+            }
+            System.out.println ();
+        }
+    }  
+}
